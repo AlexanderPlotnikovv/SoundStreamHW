a13fda5b8d9aeb9799f071c58ca3408fced875a1
Alex Plotnikov <applotnikov@edu.hse.ru>
2026-05-16 00:06:20 +0300

    Trainer updated

--- src/trainer/trainer.py
+++ src/trainer/trainer.py
@@ -1,79 +1,114 @@
+import torch
+
+from src.loss.losses import DiscriminatorLoss
 from src.metrics.tracker import MetricTracker
 from src.trainer.base_trainer import BaseTrainer
 
 
 class Trainer(BaseTrainer):
-    """
-    Trainer class. Defines the logic of batch logging and processing.
-    """
+    def __init__(
+        self,
+        model,
+        discriminator,
+        criterion,
+        metrics,
+        optimizer,
+        optimizer_d,
+        lr_scheduler,
+        config,
+        device,
+        dataloaders,
+        logger,
+        writer,
+        **kwargs,
+    ):
+        super().__init__(
+            model=model,
+            criterion=criterion,
+            metrics=metrics,
+            optimizer=optimizer,
+            lr_scheduler=lr_scheduler,
+            config=config,
+            device=device,
+            dataloaders=dataloaders,
+            logger=logger,
+            writer=writer,
+            **kwargs,
+        )
+        self.discriminator = discriminator.to(device)
+        self.optimizer_d = optimizer_d
+        self.disc_loss = DiscriminatorLoss()
 
     def process_batch(self, batch, metrics: MetricTracker):
-        """
-        Run batch through the model, compute metrics, compute loss,
-        and do training step (during training stage).
-
-        The function expects that criterion aggregates all losses
-        (if there are many) into a single one defined in the 'loss' key.
-
-        Args:
-            batch (dict): dict-based batch containing the data from
-                the dataloader.
-            metrics (MetricTracker): MetricTracker object that computes
-                and aggregates the metrics. The metrics depend on the type of
-                the partition (train or inference).
-        Returns:
-            batch (dict): dict-based batch containing the data from
-                the dataloader (possibly transformed via batch transform),
-                model outputs, and losses.
-        """
         batch = self.move_batch_to_device(batch)
-        batch = self.transform_batch(batch)  # transform batch on device -- faster
+        batch = self.transform_batch(batch)
+        x_real = batch["audio"]
 
-        metric_funcs = self.metrics["inference"]
         if self.is_train:
-            metric_funcs = self.metrics["train"]
+            x_fake, commit_loss, perplexity = self.model(x_real)
+            batch["x_fake"] = x_fake
+            batch["x_real"] = x_real
+            self.optimizer_d.zero_grad()
+
+            wave_logits_real, wave_feats_real = self.discriminator.wave_discriminator(
+                x_real
+            )
+            stft_logits_real = self.discriminator.stft_discriminator(x_real)
+            wave_logits_fake, _ = self.discriminator.wave_discriminator(x_fake.detach())
+            stft_logits_fake = self.discriminator.stft_discriminator(x_fake.detach())
+            real_logits = wave_logits_real + [stft_logits_real]
+            fake_logits = wave_logits_fake + [stft_logits_fake]
+
+            d_losses = self.disc_loss(real_logits, fake_logits)
+            d_losses["loss"].backward()
+            self.optimizer_d.step()
             self.optimizer.zero_grad()
 
-        outputs = self.model(**batch)
-        batch.update(outputs)
-
-        all_losses = self.criterion(**batch)
-        batch.update(all_losses)
-
-        if self.is_train:
-            batch["loss"].backward()  # sum of all losses is always called loss
+            wave_logits_fake, wave_feats_fake = self.discriminator.wave_discriminator(
+                x_fake
+            )
+            stft_logits_fake = self.discriminator.stft_discriminator(x_fake)
+            fake_logits = wave_logits_fake + [stft_logits_fake]
+            g_losses = self.criterion(
+                x_real=x_real,
+                x_fake=x_fake,
+                fake_logits=fake_logits,
+                real_features=wave_feats_real,
+                fake_features=wave_feats_fake,
+                commit_loss=commit_loss,
+            )
+            g_losses["loss"].backward()
             self._clip_grad_norm()
             self.optimizer.step()
-            if self.lr_scheduler is not None:
-                self.lr_scheduler.step()
 
-        # update metrics for each loss (in case of multiple losses)
-        for loss_name in self.config.writer.loss_names:
-            metrics.update(loss_name, batch[loss_name].item())
+            all_losses = {**g_losses, **d_losses}
+            for loss_name, loss_val in all_losses.items():
+                if loss_name in metrics.keys():
+                    metrics.update(loss_name, loss_val.item())
 
-        for met in metric_funcs:
-            metrics.update(met.name, met(**batch))
+            metrics.update("codebook_perplexity", perplexity.detach().item())
+            batch["loss"] = g_losses["loss"]
+
+        else:
+            with torch.no_grad():
+                x_fake, commit_loss, perplexity = self.model(x_real)
+            batch["x_fake"] = x_fake
+            batch["x_real"] = x_real
+            batch["loss"] = torch.tensor(0.0)
+
+            for met in self.metrics["inference"]:
+                metrics.update(met.name, met(x_real=x_real, x_fake=x_fake))
+
         return batch
 
     def _log_batch(self, batch_idx, batch, mode="train"):
-        """
-        Log data from batch. Calls self.writer.add_* to log data
-        to the experiment tracker.
-
-        Args:
-            batch_idx (int): index of the current batch.
-            batch (dict): dict-based batch after going through
-                the 'process_batch' function.
-            mode (str): train or inference. Defines which logging
-                rules to apply.
-        """
-        # method to log data from you batch
-        # such as audio, text or images, for example
-
-        # logging scheme might be different for different partitions
-        if mode == "train":  # the method is called only every self.log_step steps
-            # Log Stuff
-            pass
-        else:
-            # Log Stuff
-            pass
+        self.writer.add_audio(
+            "real_audio",
+            batch["x_real"][0].squeeze(0).detach().cpu(),
+            sample_rate=16000,
+        )
+        self.writer.add_audio(
+            "fake_audio",
+            batch["x_fake"][0].squeeze(0).detach().cpu(),
+            sample_rate=16000,
+        )
