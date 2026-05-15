30955d3de14d5aeade6e205fffa88f29a8ab74a0
Alex Plotnikov <applotnikov@edu.hse.ru>
2026-05-15 17:15:28 +0300

    Wave discriminator added weight normalization

--- src/model/wave_discriminator.py
+++ src/model/wave_discriminator.py
@@ -1,5 +1,5 @@
-import torch
 import torch.nn as nn
+from torch.nn.utils import weight_norm
 
 
 class ScaleDiscriminator(nn.Module):
@@ -7,13 +7,25 @@ class ScaleDiscriminator(nn.Module):
         super().__init__()
         self.layers = nn.ModuleList(
             [
-                nn.Conv1d(1, 16, kernel_size=15, padding=7),
-                nn.Conv1d(16, 64, kernel_size=41, stride=4, groups=4, padding=20),
-                nn.Conv1d(64, 256, kernel_size=41, stride=4, groups=16, padding=20),
-                nn.Conv1d(256, 1024, kernel_size=41, stride=4, groups=64, padding=20),
-                nn.Conv1d(1024, 1024, kernel_size=41, stride=4, groups=256, padding=20),
-                nn.Conv1d(1024, 1024, kernel_size=5, padding=2),
-                nn.Conv1d(1024, 1, kernel_size=3, padding=1),
+                weight_norm(nn.Conv1d(1, 16, kernel_size=15, padding=7)),
+                weight_norm(
+                    nn.Conv1d(16, 64, kernel_size=41, stride=4, groups=4, padding=20)
+                ),
+                weight_norm(
+                    nn.Conv1d(64, 256, kernel_size=41, stride=4, groups=16, padding=20)
+                ),
+                weight_norm(
+                    nn.Conv1d(
+                        256, 1024, kernel_size=41, stride=4, groups=64, padding=20
+                    )
+                ),
+                weight_norm(
+                    nn.Conv1d(
+                        1024, 1024, kernel_size=41, stride=4, groups=256, padding=20
+                    )
+                ),
+                weight_norm(nn.Conv1d(1024, 1024, kernel_size=5, padding=2)),
+                weight_norm(nn.Conv1d(1024, 1, kernel_size=3, padding=1)),
             ]
         )
         self.act = nn.LeakyReLU(0.2)
@@ -25,9 +37,7 @@ class ScaleDiscriminator(nn.Module):
             if i < len(self.layers) - 1:
                 x = self.act(x)
             feature_maps.append(x)
-
-        x = x.squeeze(1)
-        return x, feature_maps
+        return x.squeeze(1), feature_maps
 
 
 class WaveDiscriminator(nn.Module):
